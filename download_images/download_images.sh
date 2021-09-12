58583d8ca596ce1f2347fb64002f269e890947dd
Romain Beaumont <romain.rom1@gmail.com>
2021-09-12 20:53:52 +0200

    adapt download_images.sh for the exact command to run

--- download_images/download_images.sh
+++ download_images/download_images.sh
@@ -1,4 +1,5 @@
-img2dataset --url_list /media/hd/testing/cah_2parts --input_format "parquet"\
+# files from https://the-eye.eu/public/AI/cah/laion400m-met-release/laion400m-meta/
+img2dataset --url_list laion400m-meta --input_format "parquet"\
          --url_col "URL" --caption_col "TEXT" --output_format webdataset\
            --output_folder /media/hd/testing/tmp_test --processes_count 16 --thread_count 128 --image_size 256\
              --save_additional_columns '["NSFW","similarity","LICENSE"]'
