45f0e6c186135a7bd6947b6bb6940f45145f90c7
Romain Beaumont <romain.rom1@gmail.com>
2021-09-12 20:55:48 +0200

    Update download_images.sh

--- download_images/download_images.sh
+++ download_images/download_images.sh
@@ -1,5 +1,5 @@
 # files from https://the-eye.eu/public/AI/cah/laion400m-met-release/laion400m-meta/
 img2dataset --url_list laion400m-meta --input_format "parquet"\
          --url_col "URL" --caption_col "TEXT" --output_format webdataset\
-           --output_folder /media/hd/testing/tmp_test --processes_count 16 --thread_count 128 --image_size 256\
+           --output_folder your_output_folder --processes_count 16 --thread_count 128 --image_size 256\
              --save_additional_columns '["NSFW","similarity","LICENSE"]'
