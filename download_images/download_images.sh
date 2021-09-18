1e45ffe9bf8f66f8e1722b079a9ae97d23932fda
Romain Beaumont <romain.rom1@gmail.com>
2021-09-18 19:58:27 +0200

    add enable_wandb in download command

--- download_images/download_images.sh
+++ download_images/download_images.sh
@@ -2,4 +2,4 @@
 img2dataset --url_list laion400m-meta --input_format "parquet"\
          --url_col "URL" --caption_col "TEXT" --output_format webdataset\
            --output_folder your_output_folder --processes_count 16 --thread_count 128 --image_size 256\
-             --save_additional_columns '["NSFW","similarity","LICENSE"]'
+             --save_additional_columns '["NSFW","similarity","LICENSE"]' --enable_wandb True
