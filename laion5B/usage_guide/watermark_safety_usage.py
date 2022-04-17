272b23825af70f2db4f19cc5d6e729fdec7bb0af
Romain Beaumont <romain.rom1@gmail.com>
2022-04-17 17:57:37 +0200

    example of use of the watermark and safety tags for training
    using static_ondisk_kv

---
+++ laion5B/usage_guide/watermark_safety_usage.py
@@ -0,0 +1,15 @@
+import mmh3
+from static_ondisk_kv import OnDiskKV
+
+def compute_hash(url, text):
+  if url is None:
+    url = ''
+  if text is None:
+    text = ''
+  total = (url + text).encode("utf-8")
+  return mmh3.hash64(total)[0]
+
+# https://huggingface.co/datasets/laion/laion5B-watermark-safety-ordered
+
+kv = OnDiskKV(file="/media/nvme/laion5B-watermark-safety-ordered/laion5B-watermark-safety-ordered", key_format="q", value_format="ee")
+print(kv[compute_hash("http://i0.wp.com/ajournalofmusicalthings.com/wp-content/uploads/2016/05/Banjoe-the-Bulldog-copy.jpg?resize=290%2C166", "Banjoe the Bulldog copy")])
